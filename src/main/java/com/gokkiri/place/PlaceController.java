e2063f534795082c0b2634e1dc4a0b668945e787
yeoki <yeoki@DESKTOP-3GJM9N6>
2016-12-12 14:25:50 +0900

    test123

--- src/main/java/com/gokkiri/place/PlaceController.java
+++ src/main/java/com/gokkiri/place/PlaceController.java
@@ -1,7 +1,7 @@
 package com.gokkiri.place;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMapping; 
 
 @Controller
 @RequestMapping("/placeboard")
